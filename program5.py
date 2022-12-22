c0010b49405d77e078c69d4acc130fcfc0e1dcd2
Alexander Gursky <117600966+alexandergursky@users.noreply.github.com>
2022-12-22 11:19:45 -0500

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,24 @@
+
+import tkinter as tk
+import time
+
+root = tk.Tk()
+root.geometry("500x200")
+
+time_limit = 30
+start_time = time.time()
+
+
+while True:
+    # Check if the time limit has been reached
+    elapsed_time = time.time() - start_time
+    if elapsed_time > time_limit:
+        break
+    
+    root.title("Program5 Elapsed Time: {:.2f} seconds".format(elapsed_time))
+
+    # Update the UI and process events
+    root.update()
+
+# Close the window
+root.destroy()
